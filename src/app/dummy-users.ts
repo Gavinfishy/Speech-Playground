3dd1020bc8f7788f68f4516146b1de09d9ebff70
GavinDFisherMEDITECH <gfisher@meditech.com>
2025-07-17 15:52:06 -0500

    added dummy users

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,52 @@
+export const DUMMY_USERS = [
+    {
+        birthday: '01-01-2001',
+        firstName: 'Liam',
+        lastName: 'Noah',
+        middleName: 'Oliver',
+        state: 'KY',
+        city: 'Louisville',
+        address: '6007 Applegate Lane',
+        zip: '40219'
+    },
+    {
+        birthday: '02-02-2002',
+        firstName: 'Theodore',
+        lastName: 'James',
+        middleName: 'Henry',
+        state: 'CA',
+        city: 'Grass Valley',
+        address: '560 Penstock Drive',
+        zip: '95945'
+    },
+    {
+        birthday: '03-03-2003',
+        firstName: 'Olivia',
+        lastName: 'Emma',
+        middleName: 'Amelia',
+        state: 'CT',
+        city: 'Manchester',
+        address: '150 Carter Street',
+        zip: '06040'
+    },
+    {
+        birthday: '04-04-2004',
+        firstName: 'Charlotte',
+        lastName: 'Mia',
+        middleName: 'Sophia',
+        state: 'VT',
+        city: 'Essex',
+        address: '18 Densmore Drive',
+        zip: '05452'
+    },
+    {
+        birthday: '05-05-2005',
+        firstName: 'Mateo',
+        lastName: 'Elijah',
+        middleName: 'Lucas',
+        state: 'AZ',
+        city: 'Glendale',
+        address: '5601 West Crocus Drive',
+        zip: '85306'
+    },
+]
